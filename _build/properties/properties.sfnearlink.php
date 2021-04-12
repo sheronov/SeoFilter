742a65def95fc798d2b5b7d4b4738f0abe007288
Евгений Шеронов <flashboy@mail.ru>
2021-04-13 00:28:23 +0300

    warning fix

---
+++ _build/properties/properties.sfnearlink.php
@@ -0,0 +1,17 @@
+<?php
+
+$properties = array();
+
+$tmp = array();
+
+foreach ($tmp as $k => $v) {
+    $properties[] = array_merge(
+        array(
+            'name' => $k,
+            'desc' => PKG_NAME_LOWER . '_prop_' . $k,
+            'lexicon' => PKG_NAME_LOWER . ':properties',
+        ), $v
+    );
+}
+
+return $properties;
